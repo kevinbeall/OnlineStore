e0acc78bd1fa5421c3fdfce5c2a24881560706b5
Kevin <KevinAndHayley@kevins-imac.home>
2020-03-11 12:04:57 +0000

    Updated social section CSS to be responsive

--- forth-a-f/src/Components/SocialComponent/SocialComponent.js
+++ forth-a-f/src/Components/SocialComponent/SocialComponent.js
@@ -12,34 +12,73 @@ const StyledDiv = Styled.div`
   &.hidden {
     display: none;
   }
+
+  @media(min-width: 1200px) {
+    display: grid;
+    grid-template-rows: 100px 1fr;
+  }
+
 `
 
 const StyledHeading = Styled.h3`
   color: #EBDEBC;
   text-align: center;
   font-size: 1.4em;
+
+  @media(min-width: 1200px) {
+    font-size: 2em;
+  }
 `
 
+const StyledSection = Styled.div`
+  @media(min-width: 1200px){
+    padding: 50px;
+    align-self: center;
+  }
+
+  @media(max-width: 1199px){
+  &.socialTags {
+    display: none;
+  }
+}
+`;
+
+const StyledGridWrapper = Styled.div`
+  @media(min-width: 1200px){
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+`;
 
 
 const SocialComponent = (props) => {
   return (
     <StyledDiv className="SocialFeed">
-      <StyledHeading>Follow us on Instagram!</StyledHeading>
-      <InstagramEmbed
-        url='https://www.instagram.com/p/B9b3IOWAJLV/'
-        maxWidth={300}
-        hideCaption={false}
-        containerTagName='div'
-        protocol=''
-        injectScript
-        onLoading={() => { }}
-        onSuccess={() => { }}
-        onAfterRender={() => { }}
-        onFailure={() => {
-          document.querySelector('.SocialFeed').classList.add('hidden');
-        }}
-      />
+      <StyledHeading>Follow us on Social Media</StyledHeading>
+      <StyledGridWrapper>
+        <StyledSection>
+          <InstagramEmbed
+            url='https://www.instagram.com/p/B9b3IOWAJLV/'
+            maxWidth={300}
+            hideCaption={false}
+            containerTagName='div'
+            protocol=''
+            injectScript
+            onLoading={() => { }}
+            onSuccess={() => { }}
+            onAfterRender={() => { }}
+            onFailure={() => {
+              document.querySelector('.SocialFeed').classList.add('hidden');
+            }}
+          />
+        </StyledSection>
+        <StyledSection className="socialTags">
+          <StyledHeading>@forthavenuefragrance</StyledHeading>
+          <StyledHeading>On Instagram</StyledHeading>
+          <StyledHeading>And</StyledHeading>
+          <StyledHeading>Facebook</StyledHeading>
+        </StyledSection>
+      </StyledGridWrapper>
     </StyledDiv >
   );
 }
