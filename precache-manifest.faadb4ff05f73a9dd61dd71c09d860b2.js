45d73f85c3ef0e53f6f42abe64c506bee7fcdfb9
Kevin <KevinAndHayley@kevins-imac.home>
2020-03-17 12:03:41 +0000

    Updates

---
+++ precache-manifest.faadb4ff05f73a9dd61dd71c09d860b2.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9bc93d9ccf0bb34994bbcaf50449795",
+    "url": "/OnlineStore/index.html"
+  },
+  {
+    "revision": "a81e9287c350097c34cc",
+    "url": "/OnlineStore/static/css/main.375b027b.chunk.css"
+  },
+  {
+    "revision": "4f7f836f2ef40fc14a6b",
+    "url": "/OnlineStore/static/js/2.8ec05510.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/OnlineStore/static/js/2.8ec05510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a81e9287c350097c34cc",
+    "url": "/OnlineStore/static/js/main.c43b3457.chunk.js"
+  },
+  {
+    "revision": "ecebb573362cfd565aa5",
+    "url": "/OnlineStore/static/js/runtime-main.d656d719.js"
+  },
+  {
+    "revision": "2ea4c43614e5b0beff1d450e9d280ea2",
+    "url": "/OnlineStore/static/media/001-search.2ea4c436.svg"
+  },
+  {
+    "revision": "783bbdc41424fd798301dd80b851ca57",
+    "url": "/OnlineStore/static/media/002-shopping-cart.783bbdc4.svg"
+  },
+  {
+    "revision": "59f828115a64e7cef72c3b7472dbdb2a",
+    "url": "/OnlineStore/static/media/AlienInvasion.59f82811.jpg"
+  },
+  {
+    "revision": "4c79bda0c059a8a7f3e5d64e9f520547",
+    "url": "/OnlineStore/static/media/BabyPowder.4c79bda0.jpg"
+  },
+  {
+    "revision": "588bd3756d5032796adcfba479de9b78",
+    "url": "/OnlineStore/static/media/BlackOpium.588bd375.jpg"
+  },
+  {
+    "revision": "2741a54a9c1aedf412bcecaea04eb79a",
+    "url": "/OnlineStore/static/media/CountryGarden.2741a54a.jpg"
+  },
+  {
+    "revision": "99e59f7d57bbc81b087f81872b7e03c1",
+    "url": "/OnlineStore/static/media/DSC00756.99e59f7d.JPG"
+  },
+  {
+    "revision": "dbe3823ca327bbdf1a851f1f02cf2c40",
+    "url": "/OnlineStore/static/media/DSC00764.dbe3823c.JPG"
+  },
+  {
+    "revision": "b30d139d6322b18c88a680c8f04632f3",
+    "url": "/OnlineStore/static/media/DSC00767.b30d139d.JPG"
+  },
+  {
+    "revision": "d9252a004da1c0e02a471217c2708105",
+    "url": "/OnlineStore/static/media/EnglishPearFreesia.d9252a00.jpg"
+  },
+  {
+    "revision": "afd097dd4cd90443f1cff23b17608830",
+    "url": "/OnlineStore/static/media/KirRoyale.afd097dd.jpg"
+  },
+  {
+    "revision": "61fa12be5b5173cbb03a40e8f8e4d11f",
+    "url": "/OnlineStore/static/media/LaVieEstBelle.61fa12be.jpg"
+  },
+  {
+    "revision": "e0189e6ed4b7459993935485d6d8233a",
+    "url": "/OnlineStore/static/media/LadyMillionaire.e0189e6e.jpg"
+  },
+  {
+    "revision": "4a058db7edbfb17b34278ae63e2e9a7d",
+    "url": "/OnlineStore/static/media/MadamCoco.4a058db7.jpg"
+  },
+  {
+    "revision": "99422ab1fd1d6e8ab5dd57111558d02f",
+    "url": "/OnlineStore/static/media/MothersDayBox.99422ab1.JPG"
+  },
+  {
+    "revision": "99422ab1fd1d6e8ab5dd57111558d02f",
+    "url": "/OnlineStore/static/media/MothersDayBox.99422ab1.jpg"
+  },
+  {
+    "revision": "75e478fa61ed3644a73669bc1966abf0",
+    "url": "/OnlineStore/static/media/MountainAir.75e478fa.jpg"
+  },
+  {
+    "revision": "51ddcda56fde31f8091d96b45aa3ab2b",
+    "url": "/OnlineStore/static/media/Peony&Blush.51ddcda5.jpg"
+  },
+  {
+    "revision": "1ece71700246cc3ec1d94d82951c686e",
+    "url": "/OnlineStore/static/media/Sage&SeaSalt.1ece7170.jpg"
+  },
+  {
+    "revision": "f8575dc0213286ea02d42cd487af1d1f",
+    "url": "/OnlineStore/static/media/Springtime.f8575dc0.jpg"
+  },
+  {
+    "revision": "053e43a943b1818859ea77c8f825c44d",
+    "url": "/OnlineStore/static/media/freshLinen.053e43a9.jpg"
+  },
+  {
+    "revision": "a1e9b9e4129484c9ee48ffe445dbd732",
+    "url": "/OnlineStore/static/media/limeBasil&Mandarin.a1e9b9e4.jpg"
+  },
+  {
+    "revision": "971aae2e27cd8edf0d5c314bba92ef53",
+    "url": "/OnlineStore/static/media/snapBarProd.971aae2e.JPG"
+  },
+  {
+    "revision": "45a37aed7b98b3948d22fee0f3a33be9",
+    "url": "/OnlineStore/static/media/springAwakening.45a37aed.jpg"
+  },
+  {
+    "revision": "dc7c5ae79e4213d0d7d72cf6ab715e20",
+    "url": "/OnlineStore/static/media/twilightGarden.dc7c5ae7.jpg"
+  }
+]);
